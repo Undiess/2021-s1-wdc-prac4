e044b6d66de6de673b96e64aff9456c217382d76
Undiess <nguyen.andy67@gmail.com>
2021-04-04 10:51:46 +0930

    3.1

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,10 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+lastVisit = ""
+router.get('/last.txt', function(req,res){
+  res.send(lastVisit)
+   lastVisit = new Date
+})
 
 module.exports = router;
