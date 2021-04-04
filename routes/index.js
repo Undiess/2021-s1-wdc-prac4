f2dd4998908eae905a3bceca2adf0942690597f0
Undiess <nguyen.andy67@gmail.com>
2021-04-04 11:21:37 +0930

    asd

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+const router = express.Router()
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-var lastVisit;
-router.get('/last.txt', function(req,res){
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Express' })
+})
+let lastVisit
+router.get('/last.txt', function (req, res) {
   res.send(lastVisit)
-   lastVisit = new Date
+  lastVisit = new Date()
 })
 
-module.exports = router;
+module.exports = router
