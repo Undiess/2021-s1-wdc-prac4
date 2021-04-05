81c6e77b7924d039bd708dfd7c03a69cce2e121e
Undiess <nguyen.andy67@gmail.com>
2021-04-05 12:06:20 +0930

    asd

--- routes/index.js
+++ routes/index.js
@@ -10,5 +10,26 @@ router.get('/last.txt', function (req, res) {
   res.send(lastVisit)
   lastVisit = new Date()
 })
+var colors = [
+  "red","yellow","green","blue"
+]
+var  index = 0;
+router.get('/color.html',function(req,res){
+   var n = getcolor()
+   res.send(`<h1 style="color:${colors[n]}">${colors[n]}<h1>`)
+})
+
+
+
+function getcolor(){ 
+  var index1 = index
+  index++ 
+  if(index1==3){
+    index = 0; 
+  }
+  return index1
+}
+
+
 
 module.exports = router
